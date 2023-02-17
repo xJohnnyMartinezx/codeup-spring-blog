5aa8848109d77dbacef75fbc53b0d3980b45a012
Johnny Martinez <mrtnz.jn@gmail.com>
2023-02-17 15:06:56 -0600

    adding intial project files

--- src/main/java/com/codeup/codeupspringblog/CodeupSpringBlogApplication.java
+++ src/main/java/com/codeup/codeupspringblog/CodeupSpringBlogApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CodeupSpringBlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CodeupSpringBlogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CodeupSpringBlogApplication.class, args);
+    }
 
 }
--- src/test/java/com/codeup/codeupspringblog/CodeupSpringBlogApplicationTests.java
+++ src/test/java/com/codeup/codeupspringblog/CodeupSpringBlogApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CodeupSpringBlogApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
