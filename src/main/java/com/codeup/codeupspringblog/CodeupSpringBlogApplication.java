69997078eb57f2cf5245b825447da87e7aae39a6
Johnny Martinez <mrtnz.jn@gmail.com>
2023-02-17 15:04:23 -0600

    added .gitignore

---
+++ src/main/java/com/codeup/codeupspringblog/CodeupSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.codeupspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeupSpringBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeupSpringBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeup/codeupspringblog/CodeupSpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.codeupspringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeupSpringBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
