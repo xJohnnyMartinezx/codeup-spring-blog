0d3fe333c6e023cf6e9722c5f7748646d30a8195
Johnny <mrtnz.jn@gmail.com>
2023-02-19 20:01:21 -0600

    Created HomeController

---
+++ src/main/java/com/codeup/codeupspringblog/web/HomeController.java
@@ -0,0 +1,15 @@
+package com.codeup.codeupspringblog.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+        @GetMapping("/home")
+        public String welcome() {
+            return "home";
+        }
+    }
+
+}
