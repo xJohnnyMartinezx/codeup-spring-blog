5248127429499a2c0f99578f789e7c8f36c9e6ee
Johnny <mrtnz.jn@gmail.com>
2023-02-19 19:37:49 -0600

    Added multiple methods to MathController

--- src/main/java/com/codeup/codeupspringblog/web/MathController.java
+++ src/main/java/com/codeup/codeupspringblog/web/MathController.java
@@ -5,12 +5,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.nio.file.Path;
+
 @Controller
 public class MathController {
 
+//    ******** Add two nums *********
     @GetMapping("/add/{num1}/and/{num2}")
     @ResponseBody
     public String add(@PathVariable int num1, @PathVariable int num2) {
-        return  "The numbers you added equal: " + (num1 + num2);
+        return  "<h1>" + "The numbers you added equal: " + (num1 + num2) + "</h1>";
     }
+
+//    ******** Subtract two nums *********
+    @GetMapping("/subtract/{num1}/and/{num2}")
+    @ResponseBody
+    public String subtract(@PathVariable int num1, @PathVariable int num2){
+        return "<h1>" + "The difference between the two numbers is: " + (num1 - num2) + "</h1>";
+    }
+
+//    ******** Multiply two nums *********
+    @GetMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public String multiply(@PathVariable int num1, @PathVariable int num2){
+        return "<h1>" + "The product of the two numbers is: " + (num1 * num2) + "</h1>";
+    }
+
+    //    ******** Divide two nums *********
+    @GetMapping("/divide/{num1}/and/{num2}")
+    @ResponseBody
+    public String divide(@PathVariable int num1, @PathVariable int num2){
+        return "<h1>" + "The division of the two numbers is: " + (num1 / num2) + "</h1>";
+    }
+
 }
