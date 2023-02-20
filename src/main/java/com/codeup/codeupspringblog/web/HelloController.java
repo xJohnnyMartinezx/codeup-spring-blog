05d880765c9232156fe559df75fbb783aba7881c
Johnny <mrtnz.jn@gmail.com>
2023-02-19 19:14:05 -0600

    Restructured project

--- src/main/java/com/codeup/codeupspringblog/web/HelloController.java
+++ src/main/java/com/codeup/codeupspringblog/web/HelloController.java
@@ -1,12 +1,14 @@
+package com.codeup.codeupspringblog.web;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HomeController {
-    @GetMapping("/")
+public class HelloController {
+    @GetMapping("/hello")
     @ResponseBody
     public String hello(){
-        return "This is the landing page!";
+        return "<h1>" + "This is the landing page!" + "</h1>";
     }
 }
--- src/main/java/com/codeup/codeupspringblog/web/MathController.java
+++ src/main/java/com/codeup/codeupspringblog/web/MathController.java
@@ -1,3 +1,5 @@
+package com.codeup.codeupspringblog.web;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
