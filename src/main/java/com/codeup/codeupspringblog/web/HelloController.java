8242b040db2c8d7b77da015f8358bd5731b762cc
Johnny <mrtnz.jn@gmail.com>
2023-02-19 20:15:58 -0600

    Modified HelloController and created hello.html

--- src/main/java/com/codeup/codeupspringblog/web/HelloController.java
+++ src/main/java/com/codeup/codeupspringblog/web/HelloController.java
@@ -1,14 +1,17 @@
 package com.codeup.codeupspringblog.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
-    @ResponseBody
-    public String hello(){
-        return "<h1>" + "This is the landing page!" + "</h1>";
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
     }
+
 }
--- src/main/java/com/codeup/codeupspringblog/web/HomeController.java
+++ src/main/java/com/codeup/codeupspringblog/web/HomeController.java
@@ -11,5 +11,3 @@ public class HomeController {
             return "home";
         }
     }
-
-}
