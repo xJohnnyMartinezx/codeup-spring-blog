46dc76a4b98e551b9c7b810a4bfbf06cd5f59a8f
Johnny Martinez <mrtnz.jn@gmail.com>
2023-02-19 18:31:07 -0600

    Created MathController

---
+++ src/main/java/MathController.java
@@ -0,0 +1,14 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public String add(@PathVariable int num1, @PathVariable int num2) {
+        return  "The numbers you added equal: " + (num1 + num2);
+    }
+}
