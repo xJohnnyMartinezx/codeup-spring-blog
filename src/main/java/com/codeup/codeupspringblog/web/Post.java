2ea9e884d6128de4456bfb5b1aa88edf2ab21ccf
Johnny Martinez <mrtnz.jn@gmail.com>
2023-02-22 10:27:22 -0600

    Created Post class and accompanying htmls files.

---
+++ src/main/java/com/codeup/codeupspringblog/web/Post.java
@@ -0,0 +1,28 @@
+package com.codeup.codeupspringblog.web;
+
+public class Post {
+
+
+//    ****** Properties ********
+    private String title;
+    private String body;
+
+//    ******* Getters and Setters ********
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
