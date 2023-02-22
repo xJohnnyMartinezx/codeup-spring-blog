c20de55b89cf60e7175278c2fb88a2af1a6658bc
Johnny Martinez <mrtnz.jn@gmail.com>
2023-02-22 15:05:28 -0600

    Created post-form.html for creating a new post and modified my post method.

--- src/main/java/com/codeup/codeupspringblog/web/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/web/PostController.java
@@ -2,10 +2,7 @@ package com.codeup.codeupspringblog.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,11 +48,12 @@ public class PostController {
         return "posts/posts-form";
     }
 
-    //    ****** Create a new post *******
-//    @PostMapping("/posts/create")
-//    @ResponseBody
-//    public String createNewPost(){
-//        return "posts";
-//    }
+//        ****** Create a new post *******
+    @PostMapping("/posts/create")
+    public String createNewPost(@RequestParam(name = "title") String title, @RequestParam String body){
+        Post newPost = new Post(posts.size() + 1, title, body);
+        posts.add(newPost);
+        return "redirect:/posts/show";
+    }
 
 }
