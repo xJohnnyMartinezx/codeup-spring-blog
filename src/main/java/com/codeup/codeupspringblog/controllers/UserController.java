4018cad8a01ecb1a7d4a841ad4fb71b2aac0e5fc
Johnny Martinez <mrtnz.jn@gmail.com>
2023-02-28 14:36:10 -0600

    Created UserRepo and UserController.

---
+++ src/main/java/com/codeup/codeupspringblog/controllers/UserController.java
@@ -0,0 +1,5 @@
+package com.codeup.codeupspringblog.controllers;
+
+public class UserController {
+
+}
---
+++ src/main/java/com/codeup/codeupspringblog/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.codeup.codeupspringblog.repositories;
+
+import com.codeup.codeupspringblog.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
