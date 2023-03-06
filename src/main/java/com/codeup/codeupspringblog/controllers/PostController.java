4bf55f54ef6a748a82b273bd9d47da3e033a48b2
Johnny Martinez <mrtnz.jn@gmail.com>
2023-03-06 13:18:10 -0600

    origanized PostController and added comments. Also, linked edit button on individual-post.html to redirect to edit-post-form.html on click.

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -12,59 +12,53 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class PostController {
 
-//    ***** DEPENDENCY INJECTION FIELD/S *****
+    //    ***** DEPENDENCY INJECTION FIELD/S *****
     private PostRepository postRepository;
     private UserRepository userRepository;
 
-//    ***** CONSTRUCTORS *******
-
+    //    ***** CONSTRUCTORS *******
     public PostController(UserRepository userRepository, PostRepository postRepository) {
         this.userRepository = userRepository;
         this.postRepository = postRepository;
     }
 
-//    ****** View all posts *******
+    //    ****** View all posts *******
     @GetMapping("/posts/show")
-    public String viewAllPosts(Model model){
+    public String viewAllPosts(Model model) {
+    //   USING MODEL BINDING TO GET A LIST OF ALL POST BY USING POST REPO findAll() METHOD. ATTRIBUTE NAME IS: postList.
         model.addAttribute("postList", postRepository.findAll());
         return "posts/show";
     }
 
-    //    ****** View a single designated post *******
-//    @GetMapping("/posts/index")
-//    public String viewSinglePost(Model model){
-//
-//
-//        model.addAttribute("firstPost", postDao.);
-//
-//        return "posts/index";
-//    }
-
     //    ****** View form for creating a post (MODEL BIDING) *******
     @GetMapping("/posts/create")
-    public String createPostsForm(Model model){
+    public String createPostsForm(Model model) {
         model.addAttribute("post", new Post());
         return "posts/create-posts-form";
     }
 
-//        ****** Create a new post (MODEL BIDING) *******
+    //    ****** Create a new post (MODEL BIDING & ASSIGNED USER MANUALLY) *******
     @PostMapping("/posts/create")
     public String createNewPost(@ModelAttribute Post post) {
+//        MANUALLY GETTING/PICKING A USER
         User user = userRepository.getById(1L);
+//        ASSIGNING THE CREATED POST TO THE USER
         post.setUser(user);
+//        SAVING THE CREATED POST TO POSTS REPO
         postRepository.save(post);
+//        REDIRECTING TO VIEW ALL POSTS PAGE
         return "redirect:/posts/show";
     }
 
-//        ****** Edit an existing Post *******
-        @GetMapping("/posts/{id}/edit")
-        public String editPostForm(@PathVariable long id, Model model){
-            model.addAttribute("indiPost", postRepository.findById(id).get());
-                    return "posts/edit-post";
-        }
+    //        ****** Edit an existing Post *******
+    @GetMapping("/posts/{id}/edit")
+    public String editPostForm(@PathVariable long id, Model model) {
+        model.addAttribute("indiPost", postRepository.findById(id).get());
+        return "posts/edit-post-form";
+    }
 
     @PostMapping("/posts/{id}/edit")
-    public String editPost(@PathVariable long id, @ModelAttribute Post post){
+    public String editPost(@PathVariable long id, @ModelAttribute Post post) {
         postRepository.save(post);
         return "redirect:/posts/" + id;
     }
@@ -89,7 +83,7 @@ public class PostController {
 //       ****** View posts by ID *******
 
     @GetMapping("post/individual-post")
-    public String viewPostById(){
+    public String viewPostById() {
         return "posts/individual-post";
     }
 
@@ -98,8 +92,6 @@ public class PostController {
         model.addAttribute("indiPost", postRepository.findById(id).get());
         return "posts/individual-post";
     }
-
-
 
 
 }
