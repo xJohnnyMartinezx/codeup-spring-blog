705a8bc5f63feb919aee67a6187186fb1742cd9c
Johnny <mrtnz.jn@gmail.com>
2023-03-07 09:40:32 -0600

    restructured project

--- src/main/java/com/codeup/codeupspringblog/repositories/PostRepository.java
+++ src/main/java/com/codeup/codeupspringblog/repositories/PostRepository.java
@@ -2,6 +2,8 @@ package com.codeup.codeupspringblog.repositories;
 
 import com.codeup.codeupspringblog.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
--- src/main/java/com/codeup/codeupspringblog/repositories/UserRepository.java
+++ src/main/java/com/codeup/codeupspringblog/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.codeup.codeupspringblog.repositories;
 
 import com.codeup.codeupspringblog.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
